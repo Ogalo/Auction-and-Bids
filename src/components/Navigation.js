43d37c9fa416cce35a91c424e59c53d2d846e2e3
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-09-09 00:32:47 +0300

    Add file components

---
+++ src/components/Navigation.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function Navigation() {
+  return (
+    <div className="Nav">
+      <header className="Nav-header">
+      <section className="Brand-Name">
+      <h1>Auctions and Bids</h1>
+      </section>
+      <section className="Nav-Bar">
+      <ul>
+      <li>Home</li>
+      <li>Auction</li>
+      <li>Service</li>
+      <li>Help</li>
+      </ul>
+      </section>
+      <section className="Users">
+      <img />
+      <h2>Harrsion</h2>
+      <p>title</p>
+
+      </section>
+
+      </header>
+    </div>
+  );
+}
+
+export default Navigation;
