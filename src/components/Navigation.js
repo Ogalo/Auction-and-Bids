da1ca5dfb5c9f58e1698118961920f549105ef9a
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-09-09 00:47:19 +0300

    Add contents of navigation and Products section

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,4 @@
-import './App.css';
+import React from 'react'
 
 function Navigation() {
   return (
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,17 @@
+import React from 'react'
 
+function Products() {
+  return (
+    <>Products
+    <section className='Product'>
+    <h2>Products</h2>
+    <div>Arts</div>
+    <div>Vehicles</div>
+    <div>Houses</div>
+    <div>Others</div>
+    </section>
+    </>
+  )
+}
+
+export default Products
