b9dd2f7b97adeb5df4130dfd559bae216a883397
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-09-11 18:39:44 +0300

    Revert to the original display settings

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Products from './components/Products'
 import Auction from './components/Auction';
 import Bidders from './components/Bidders';
 import Media from './components/Media';
-// import SignUp from './components/SignUp';
-// import LogIn from './components/LogIn'
-// import Bid from './components/BidPlacement'
+import SignUp from './components/SignUp';
+import LogIn from './components/LogIn'
+import Bid from './components/BidPlacement'
 import Sell from './components/Sell'
 function App() {
   return (
     <div>
-    <div>
-
+    <div className='formPages'>
+     <SignUp />
+     <LogIn />
+     <Bid />
      <Sell />
      </div>
    <div className='main'>
