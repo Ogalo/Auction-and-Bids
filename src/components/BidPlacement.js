67f14da4e67ee995f9dd410404da8b2ccdcd63cc
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-09-13 20:42:15 +0300

    Add a fetch request in the bid placement

--- src/components/BidPlacement.js
+++ src/components/BidPlacement.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import {FaTimesCircle  } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 
 function Bid({open, close}) {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [id_no, setId] = useState();
+  const [item_name, setItemName] = useState("");
+
+  const [item_id, setItemId] = useState();
+  const [your_bid, setBid] = useState();
+
+
   if(!open){return null}
-  const handleSubmit = (e) => {
+
+  function handleSubmit(e){
     e.preventDefault();
-  };
+    fetch("http://localhost:9292/bids", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        id_no: id_no,
+        item_name: item_name,
+        item_id: item_id,
+        your_bid: your_bid,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        navigate("/");
+        console.log(data);
+      });
+  }
 
   const handleBid = () => {
     alert("Bid Successful");
@@ -20,42 +49,50 @@ function Bid({open, close}) {
             <div className="container2">
             <Link to='/'><FaTimesCircle className='closeTab' onClick={close}/></Link>
               <h1>Place Bid</h1>
-              <label htmlFor="email">
+              <label>
                 <b>Your Name:</b>
               </label>
               <input
                 type="text"
                 placeholder="First Name"
                 name="name"
+                value={name}
+                onChange={(e)=> setName(e.target.value)}
                 required
               />
-              <label htmlFor="email">
+              <label>
               <b>ID no:</b>
             </label>
             <input
               type="number"
               placeholder="Your Identity card no"
               name="name"
+              value={id_no}
+                onChange={(e)=> setId(e.target.value)}
               required
             />
 
 
-              <label htmlFor="psw">
+              <label>
                 <b>Item name:</b>
               </label>
               <input
                 type="text"
                 placeholder="Enter Item Name"
                 name="item"
+                value={item_name}
+                onChange={(e)=> setItemName(e.target.value)}
                 required
               />
-              <label htmlFor="psw">
+              <label>
               <b>Item Id:</b>
               </label>
               <input
               type="number"
               placeholder="Enter Item ID"
               name="itemId"
+              value={item_id}
+                onChange={(e)=> setItemId(e.target.value)}
               required
               />
               <label htmlFor="email">
@@ -65,6 +102,8 @@ function Bid({open, close}) {
               type="number"
               placeholder="$8400"
               name="name"
+              value={your_bid}
+                onChange={(e)=> setBid(e.target.value)}
               required
               />
 
@@ -72,7 +111,7 @@ function Bid({open, close}) {
               <button
                 type="submit"
                 className="registerbtn"
-                onClick={handleBid}
+                onClick={() => {}}
               >
                 Bid
               </button>
